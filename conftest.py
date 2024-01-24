1b68daa4c5f92f1f84ab8b41ddedd647ee0d9bed
baxnul <khafizovilnaz100@gmail.com>
2024-01-24 21:45:39 +0300

    delete multy-language in conftest.py file

--- conftest.py
+++ conftest.py
@@ -1,8 +1,6 @@
 import pytest
 from loguru import logger
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.firefox.options import Options as OptionsFirefox
 
 logger.add("./logs/test.log", format="{time} {level} {message}", level="DEBUG", rotation="10 MB", compression="zip")
 
@@ -13,24 +11,15 @@ def pytest_addoption(parser):
 
 
 @pytest.fixture(scope="function")
-@logger.catch()
 def browser(request):
     browser_name = request.config.getoption("browser_name")
-    user_language = request.config.getoption("language")
-
-    options = Options()
-    options.add_experimental_option(
-        'prefs', {'intl.accept_languages': user_language})
-
-    options_firefox = OptionsFirefox()
-    options_firefox.set_preference("intl.accept_languages", user_language)
 
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome(options=options)
+        browser = webdriver.Chrome()
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox(options=options_firefox)
+        browser = webdriver.Firefox()
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
