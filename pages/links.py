61cb763abc60ca266cb21b8b7345bafc4fdd9e63
baxnul <khafizovilnaz100@gmail.com>
2024-01-24 17:47:47 +0300

    added links.py

---
+++ pages/links.py
@@ -0,0 +1,4 @@
+class YadroUrl:
+    YADRO_MAIN_PAGE_LINK = "https://yadro.com"
+    YADRO_CAREERS_LINK = "https://careers.yadro.com"
+    YADRO_VACANCY_PAGE_LINK = "https://careers.yadro.com/vacancy/"
