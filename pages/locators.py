7e46175e5e2139a2fd7b5583dc2d2ebdf78669b4
baxnul <khafizovilnaz100@gmail.com>
2024-01-24 21:46:08 +0300

    add new locators

---
+++ pages/locators.py
@@ -0,0 +1,26 @@
+from selenium.webdriver.common.by import By
+
+""" ALL LOCATOR have selector and locator
+    Example: (Tuple have: By selector, locator)
+    and some LOCATOR have timeout wait
+    Example: (Tuple have: By selector, locator, timeout wait) """
+
+
+class BasePageLocators:
+    pass
+
+
+class MainPageLocators:
+    CAREERS_LINK = (By.CSS_SELECTOR, "[href='https://careers.yadro.com']")
+
+
+class CareersPageLocators:
+    WELCOME_SECTION = (By.CSS_SELECTOR, "[class='welcome__section d-flex']")
+    CAREERS_NAV_MENU = (By.CSS_SELECTOR, "[class='navmenu__section _top']")
+    MAIN_PAGE_LINK = (By.CSS_SELECTOR, "[imgfield='tn_img_1540301280185']")
+    CAREERS_NAV_ITEM_VACANCY = (By.ID, "menu-item-16")
+
+
+class VacancyPageLocators:
+    SELECT_TAB = (By.CSS_SELECTOR, "[aria-current='page']")
+    BREADCRUMB_LINK = (By.CSS_SELECTOR, "[class='breadcrumbs']")
