b1d16703a933a47b952dfac39d058a22574d03f3
baxnul <khafizovilnaz100@gmail.com>
2024-01-31 11:52:54 +0300

    add driver and add new options --headless and --no-sandbox

--- conftest.py
+++ conftest.py
@@ -1,5 +1,11 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as OptionsFirefox
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+service = Service(ChromeDriverManager().install())
 
 
 def pytest_addoption(parser):
@@ -11,12 +17,19 @@ def pytest_addoption(parser):
 def browser(request):
     browser_name = request.config.getoption("browser_name")
 
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument('--no-sandbox')
+
+    options_firefox = OptionsFirefox()
+    options_firefox.add_argument("--headless")
+
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+        browser = webdriver.Chrome(service=service, options=options)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
+        browser = webdriver.Firefox(options=options_firefox)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
