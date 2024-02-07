6b5219dc197795ed796db8ad600ce39e1cd32749
baxnul <khafizovilnaz100@gmail.com>
2024-02-07 11:18:31 +0300

    add new options

--- conftest.py
+++ conftest.py
@@ -19,17 +19,24 @@ def browser(request):
 
     options = Options()
     options.add_argument("--headless")
-    options.add_argument('--no-sandbox')
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
 
     options_firefox = OptionsFirefox()
     options_firefox.add_argument("--headless")
+    options_firefox.add_argument("--no-sandbox")
+    options_firefox.add_argument("--disable-dev-shm-usage")
+    options_firefox.add_argument("--window-size=1920,1080")
 
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
         browser = webdriver.Chrome(service=service, options=options)
+        request.cls.driver = browser
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
         browser = webdriver.Firefox(options=options_firefox)
+        request.cls.driver = browser
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
